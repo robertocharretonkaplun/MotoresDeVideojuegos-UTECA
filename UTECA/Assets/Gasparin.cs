fc33807d518ff879c8b4c10b8e8bd0e83f1fd212
Roberto Charreton <61713617+robertocharretonkaplun@users.noreply.github.com>
2022-10-26 18:54:33 -0500

    Parcial2 - Joints And Effectos
    Part 1

---
+++ UTECA/Assets/Gasparin.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LEVEL
+{
+  LEVEL_1,
+  LEVEL_2,
+  LEVEL_3,
+  LEVEL_4,
+  LEVEL_5,
+}
+
+public class Gasparin : MonoBehaviour
+{
+  public int level = 0;
+  public LEVEL levelenum;
+  // Start is called before the first frame update
+  void Start()
+  {
+
+  }
+
+  // Update is called once per frame
+  void Update()
+  {
+
+  }
+
+  private void OnCollisionEnter2D(Collision2D collision)
+  {
+    if (collision.gameObject.CompareTag("Fantasma"))
+    {
+      var fantasma = collision.gameObject.GetComponent<Gasparin>();
+      if (fantasma.levelenum == levelenum)
+      {
+        // Disable the Sprite Renderer
+        gameObject.SetActive(false);
+        collision.gameObject.SetActive(false);
+        // Combinar y crear un nuevo fantasma
+        var newFantasma = Instantiate(new GameObject(), transform.position, Quaternion.identity);
+        // Destruir a el objeto colisionado y a este objeto
+        Destroy(collision.gameObject);
+        Destroy(this);
+      }
+    }
+  }
+}
